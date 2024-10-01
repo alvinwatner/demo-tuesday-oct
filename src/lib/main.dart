fd9aee27462275a7b073a22ab0fb092b362dd765
Alvin Watner <58515206+alvinwatner@users.noreply.github.com>
2024-10-01 10:06:47 +0800

    Modify App: demo-tuesday-oct - Success (#2)
    * Update 1 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 15 files
    
    * Update 9 files

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'cubits/auth_cubit.dart';
+import 'cubits/stock_cubit.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 
@@ -12,8 +13,11 @@ void main() {
 class MainApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		return BlocProvider(
-			create: (_) => AuthCubit(),
+		return MultiBlocProvider(
+			providers: [
+				BlocProvider(create: (_) => AuthCubit()),
+				BlocProvider(create: (_) => StockCubit()..fetchStockData())
+			],
 			child: MaterialApp(
 				title: 'Simple App',
 				home: BlocBuilder<AuthCubit, AuthState>(
--- src/lib/screens/login_screen.dart
+++ src/lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:com.example.simple_app/widgets/login_form.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:com.example.simple_app/cubits/auth_cubit.dart';
+import 'package:com.example.simple_app/screens/home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
 	@override
@@ -19,6 +22,18 @@ class LoginScreen extends StatelessWidget {
 						),
 						SizedBox(height: 16),
 						LoginForm(),
+						SizedBox(height: 16),
+						ElevatedButton(
+							onPressed: () {
+								// Normally, you would collect the email and password from the LoginForm
+								// For simplicity, we'll use hardcoded values here
+								String email = 'user@example.com';
+								String password = 'password';
+
+								context.read<AuthCubit>().login(email, password);
+							},
+							child: Text('Login'),
+						)
 					],
 				),
 			),
